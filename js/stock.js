4655174b8fd2c0e9c015a15c031371c915c5470c
Angel San luis <ovnicrofordz@gmail.com>
2022-07-19 01:25:08 -0300

    entrega numero 9

--- js/stock.js
+++ js/stock.js
@@ -2,7 +2,7 @@ const productos = [
     {
         id:1,
         nombre:"Cafe con leche",
-        img:`/img/1.jpg`,
+        img:`./img/1.jpg`,
         descripcion:"Cafe arabico con escencia vainilla o canella",
         precio:250,
         cantidad:1,
@@ -10,7 +10,7 @@ const productos = [
     {
         id:2,
         nombre:"Frapuchino",
-        img:`/img/2.jpg`,
+        img:`./img/2.jpg`,
         descripcion:"Cafe arabico con escencia vainilla o canella",
         precio:250,
         cantidad:1,
@@ -18,7 +18,7 @@ const productos = [
     {
         id:3,
         nombre:"Expresso",
-        img:`/img/3.jpg`,
+        img:`./img/3.jpg`,
         descripcion:"Cafe arabico con escencia vainilla o canella",
         precio:250,
         cantidad:1,
@@ -26,7 +26,7 @@ const productos = [
     {
         id:4,
         nombre:"Medialunas",
-        img:`/img/4.jpg`,
+        img:`./img/4.jpg`,
         descripcion:"Cafe arabico con escencia vainilla o canella",
         precio:250,
         cantidad:1,
@@ -34,7 +34,7 @@ const productos = [
     {
         id:5,
         nombre:"Tostado",
-        img:`/img/5.jpg`,
+        img:`./img/5.jpg`,
         descripcion:"Cafe arabico con escencia vainilla o canella",
         precio:250,
         cantidad:1,
@@ -42,7 +42,7 @@ const productos = [
     {
         id:6,
         nombre:"Expresso",
-        img:`/img/6.jpg`,
+        img:`./img/6.jpg`,
         descripcion:"Cafe arabico con escencia vainilla o canella",
         precio:250,
         cantidad:1,
@@ -50,14 +50,14 @@ const productos = [
     {
         id:6,
         nombre:"Expresso",
-        img:`/img/6.jpg`,
+        img:`./img/6.jpg`,
         descripcion:"Cafe arabico con escencia vainilla o canella",
         precio:250,
         cantidad:1,
     },   {
         id:6,
         nombre:"Expresso",
-        img:`/img/6.jpg`,
+        img:`./img/6.jpg`,
         descripcion:"Cafe arabico con escencia vainilla o canella",
         precio:250,
         cantidad:1,
