46f8e55a4c305215549dfe53f5e79234a83fe88a
Angel San luis <ovnicrofordz@gmail.com>
2022-08-04 22:52:25 -0300

    arreglo suma miguel

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-import {productos} from "./js/stock.js"
+import {
+    productos
+} from "./js/stock.js"
 // Selecciono los id para poder utilizarlos 
-const abrirCarrito =document.getElementById("abrirCarrito");
-const modalCarrito =document.getElementById("modalCarrito");
+const abrirCarrito = document.getElementById("abrirCarrito");
+const modalCarrito = document.getElementById("modalCarrito");
 const botonVaciar = document.getElementById("vaciar-carrito")
-const contenidoModal =document.getElementById("contenidoModal");
+const contenidoModal = document.getElementById("contenidoModal");
 const precioTotal = document.getElementById('precioTotal')
 let carrito = []
 
 
 
 let menu = document.getElementById("menu")
-const mostrarProductos = (productos) =>{
-productos.forEach((producto)=>{
-const div = document.createElement('div')
-div.innerHTML += `<div class="card " style="width: 18rem;">
+const mostrarProductos = (productos) => {
+    productos.forEach((producto) => {
+        const div = document.createElement('div')
+        div.innerHTML += `<div class="card " style="width: 18rem;">
                     <img src="${producto.img}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${producto.nombre}</h5>
@@ -22,89 +24,93 @@ div.innerHTML += `<div class="card " style="width: 18rem;">
                         <button class="btn btn-danger" id="boton${producto.id}">Comprar</button>
                     </div>
                 </div>`
-                menu.appendChild(div)
+        menu.appendChild(div)
 
-const boton = document.getElementById(`boton${producto.id}`)
-boton.addEventListener('click',()=>{
-    agregarAlCarrito(producto.id)
-    Toastify({
-        text: `Se agrego ${producto.nombre} +$${producto.precio}`,
-        className: "degrade",
-        style: {
-        background: "linear-gradient(to right, #664343, #926657)",
-        }
-    }).showToast();
+        const boton = document.getElementById(`boton${producto.id}`)
+        boton.addEventListener('click', () => {
+            agregarAlCarrito(producto.id)
+            Toastify({
+                text: `Se agrego ${producto.nombre} +$${producto.precio}`,
+                className: "degrade",
+                style: {
+                    background: "linear-gradient(to right, #664343, #926657)",
+                }
+            }).showToast();
 
-})
-})
+        })
+    })
 }
-mostrarProductos(productos)
+mostrarProductos(productos);
+
 const agregarAlCarrito = (prodId) => {
-    const existe = carrito.some (prod => prod.id ===prodId)
-    if (existe){
-        const prod = carrito.map (prod=>{
-        if (prod.id === prodId){
-            prod.cantidad++
-        }})
-    }else { 
-    const item = productos.find((prod)=> prod.id === prodId);
-    if (item) {
-        const prod =productos.map(prod =>{
-            if (prod.id === prodId){
-                prod.cantidad = 1
+    const existe = carrito.some(prod => prod.id === prodId)
+    if (existe) {
+        const prod = carrito.map(prod => {
+            if (prod.id === prodId) {
+                prod.cantidad++
             }
         })
+    } else {
+        const item = productos.find((prod) => prod.id === prodId);
+        if (item) {
+            const prod = productos.map(prod => {
+                if (prod.id === prodId) {
+                    prod.cantidad = 1
+                }
+            })
+        }
+        carrito.push(item);
     }
-    carrito.push(item)
-}
-    actualizarCarrito()
+    actualizarCarrito();
 }
 
-const eliminarDelCarrito = (prodId) =>{
+const eliminarDelCarrito = (prodId) => {
     console.log(prodId);
-    const item = carrito.find((prod) =>prod.id === prodId)
-    const indice =carrito.indexOf(item)
-    carrito.splice(indice,1)
-    actualizarCarrito()
+    const item = carrito.find((prod) => prod.id === prodId);
+    const indice = carrito.indexOf(item);
+    carrito.splice(indice, 1);
+    actualizarCarrito();
 }
 
 
 const actualizarCarrito = () => {
-    contenidoModal.innerHTML =""
-    carrito.forEach((prod)=>{
+    contenidoModal.innerHTML = ""
+    carrito.forEach((prod) => {
         const div = document.createElement('div')
         div.className = ("modalCarrito__items")
-        div.innerHTML =`
+        div.innerHTML = `
         <img class="px-1" src="${prod.img} " alt="" width="40px" height="40px">
         <p class="px-2"> <b>${prod.nombre}</b></p>
         <p class="px-2"><b>Precio: ${prod.precio}</b></p
         <p class="px-2"><b>Cantidad: <span id ="cantidad" class="px-2">${prod.cantidad}</b> </span></p>
         <button id="id${prod.id}" class ="boton-eliminar"><i class="fas fa-trash-alt"> </i></button>`
-    
+
         contenidoModal.appendChild(div)
         let botonid = document.getElementById(`id${prod.id}`)
-        botonid.addEventListener('click',()=> eliminarDelCarrito(prod.id))
+        botonid.addEventListener('click', () => eliminarDelCarrito(prod.id))
     })
-    precioTotal.innerHTML =carrito.reduce((acc,prod)=>acc+prod.precio,0)
+    precioTotal.innerHTML = carrito.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0)
 }
 
 
 // ME LLAMO LOS BOTONES PARA LUEGO PODER ABRIR Y CERRAR EL MENU DE REGISTRO Y LOGIN 
 let registro = document.getElementById("registro");
 let login = document.getElementById("login");
-let registronone =document.getElementById("registronone");
-let loginnone =document.getElementById("loginnone");
+let registronone = document.getElementById("registronone");
+let loginnone = document.getElementById("loginnone");
 
 
-registro.addEventListener("click",aparecer)
-function aparecer(){
-registronone.classList.toggle("d-none")
-loginnone.classList.add("d-none")
+registro.addEventListener("click", aparecer)
+
+function aparecer() {
+    registronone.classList.toggle("d-none")
+    loginnone.classList.add("d-none")
 }
-login.addEventListener("click",aparecer2)
-function aparecer2(){
-loginnone.classList.toggle("d-none")
-registronone.classList.add("d-none")
+login.addEventListener("click", aparecer2)
+
+function aparecer2() {
+    loginnone.classList.toggle("d-none")
+    registronone.classList.add("d-none")
 }
 
 
@@ -112,80 +118,81 @@ registronone.classList.add("d-none")
 // LA FUNCION REGISTROFORM QUE TOMA EL VALUE Y LO GUARDA EN EL SESSION
 
 let formregistro = document.getElementById('formRegistro');
-    formregistro.addEventListener('submit',registroform);
+formregistro.addEventListener('submit', registroform);
 
-function registroform(e){
-e.preventDefault(e);
-let formulario = e.target;
+function registroform(e) {
+    e.preventDefault(e);
+    let formulario = e.target;
 
-if ((!formulario.children[2].value) || (!formulario.children[4].value))  {
-    Swal.fire({
-    icon: 'error',
-    title: 'Debe completar todos los campos!',
-})}
-else {
-sessionStorage.setItem("email",formulario.children[2].value)
-sessionStorage.setItem("clave",formulario.children[4].value)
-let formularioStorage = JSON.stringify(sessionStorage.getItem("email"))
-console.log(formularioStorage);
-Swal.fire({
-    position: 'center',
-    icon: 'success',
-    title: 'Te has registrado correctamente',
-    showConfirmButton: false,
-    timer: 1500
-}) 
-registronone.classList.toggle("d-none")}
+    if ((!formulario.children[2].value) || (!formulario.children[4].value)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Debe completar todos los campos!',
+        })
+    } else {
+        sessionStorage.setItem("email", formulario.children[2].value)
+        sessionStorage.setItem("clave", formulario.children[4].value)
+        let formularioStorage = JSON.stringify(sessionStorage.getItem("email"))
+        console.log(formularioStorage);
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Te has registrado correctamente',
+            showConfirmButton: false,
+            timer: 1500
+        })
+        registronone.classList.toggle("d-none")
+    }
 }
 
 // ME TRAIGO EL FORMULARIO LOGIN Y LUEGO DEL SUBMIT COMPARO LOS DATOS INGRESADOS EN EL SESSIONSTORE
 // CON LOS TARGET INGRESADOS EN EL FORMULARIO LOGIN 
-let formlogin= document.getElementById('formLogin');
-formlogin .addEventListener('submit',loginform);
+let formlogin = document.getElementById('formLogin');
+formlogin.addEventListener('submit', loginform);
 
-function loginform(e){
-    let formulario= e.target;
-e.preventDefault(e);
-if (((sessionStorage.getItem("email") == formulario.children[2].value)) && ((sessionStorage.getItem("clave") == formulario.children[4].value))) {
-Swal.fire({
-    position: 'center',
-    icon: 'success',
-    title: 'Ha ingresado correctamente, ahora puede hacer su pedido',
-    showConfirmButton: false,
-    timer: 1500
-})
-loginnone.classList.toggle("d-none")
+function loginform(e) {
+    let formulario = e.target;
+    e.preventDefault(e);
+    if (((sessionStorage.getItem("email") == formulario.children[2].value)) && ((sessionStorage.getItem("clave") == formulario.children[4].value))) {
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Ha ingresado correctamente, ahora puede hacer su pedido',
+            showConfirmButton: false,
+            timer: 1500
+        })
+        loginnone.classList.toggle("d-none")
 
-/* mostrarProductos(productos) */
+        /* mostrarProductos(productos) */
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Algo anduvo mal',
+            text: 'El usuario o contraseña ingresado no es valido',
+
+        })
+    }
 }
-else{
-    Swal.fire({
-        icon: 'error',
-        title: 'Algo anduvo mal',
-        text: 'El usuario o contraseña ingresado no es valido',
-
-    })
-}
-}
 
 
 
-abrirCarrito.addEventListener("click",aparecer3)
-function aparecer3(){
-modalCarrito.classList.toggle("d-none"); 
+abrirCarrito.addEventListener("click", aparecer3)
+
+function aparecer3() {
+    modalCarrito.classList.toggle("d-none");
 }
 
 
 
 let ulheader2 = document.getElementById('ulheader2')
 let hamburgesa = document.getElementById('hamburgesa')
-hamburgesa.addEventListener('click',aparecerr)
+hamburgesa.addEventListener('click', aparecerr)
 
-function aparecerr(){
+function aparecerr() {
     ulheader2.classList.toggle("d-none")
 }
 
-botonVaciar.addEventListener('click',()=> {
+botonVaciar.addEventListener('click', () => {
     carrito.length = 0
     actualizarCarrito()
 })
